f9880e4fa6df48ea7680be5becb0a14cb0ecf26b
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-08-17 17:42:00 -0500

    20

---
+++ 20.py
@@ -0,0 +1,9 @@
+a=float(input("Primer numero: "))
+b=float(input("Segundo numero: "))
+c=float(input("Tercer numero: "))
+if a<b and a<c:
+    print(a)
+elif b<a and b<c:
+    print(b)
+else:
+    print(c)
