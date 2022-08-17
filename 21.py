bad20efba497d71c12c652bcbbf8c19fdef8ef22
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-08-17 17:45:32 -0500

    21

---
+++ 21.py
@@ -0,0 +1,6 @@
+nombre=input("Nombre de usuario: ")
+password=input("Contraseña: ")
+if nombre=="Gwenevere" and password=="excalibur":
+    print("Usuario y contraseña correctos. Puede ingresar a Camelot")
+else:
+    print("Acceso denegado")
